9f7d5df4261cc211e95c8ced5ca15139471eb303
arms <arms_1991@hotmail.com>
2021-03-12 10:31:35 -0300

    removendo comentários

--- src/main/java/br/com/yoshi/projitau/repository/TransacaoRepo.java
+++ src/main/java/br/com/yoshi/projitau/repository/TransacaoRepo.java
@@ -16,24 +16,4 @@ public interface TransacaoRepo extends CrudRepository<Transacao, Integer> {
     "GROUP BY t.agFinanceiro.nome, t.status " +
     "ORDER By t.status")
     List<ContagemStatusDTO> teste(int idAgente);
-
-    // @Query(value = "SELECT new br.com.yoshi.projitau.dto.ContagemStatusDTO(t.ag_financeiro,a.nome_agente,a.volume_transacional, "+
-    // "count(case when status = 0 then 1 else 0 end) AS sucessos, " +
-    // "count(case when status = 1 then 1 else 0 end) AS falhas, " +
-    // "count(case when status = 2 then 1 else 0 end) AS fraudes) " +
-    // "FROM mtb310_transaction as t " +
-    // "RIGHT JOIN mtb310_ag_financeiro as a " +
-    // "ON(t.ag_financeiro=a.id_agente) " + 
-    // "GROUP BY t.ag_financeiro")
-    // List<ContagemStatusDTO> teste(int idAgente);
-
-    // @Query("SELECT new br.com.yoshi.projitau.dto.ContagemStatusDTO(t.agFinanceiro,a.nome,a.volumeTransacional, "+
-    // "count(case when status = 0 then 1 else 0 end) AS sucessos, " +
-    // "count(case when status = 1 then 1 else 0 end) AS falhas, " +
-    // "count(case when status = 2 then 1 else 0 end) AS fraudes) " +
-    // "FROM Transacao as t " +
-    // "RIGHT JOIN AgFinanceiro as a " +
-    // "ON(t.agFinanceiro=a.id) " + 
-    // "GROUP BY t.agFinanceiro")
-    // List<ContagemStatusDTO> teste(int idAgente);
 }
